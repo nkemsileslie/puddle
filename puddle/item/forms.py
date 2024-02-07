6ada641667cbe18712e76d9f46f9830ed360aeb4
nkemsileslie <nkemsileslie@gmail.com>
2024-02-07 05:00:07 -0500

    added items and media

--- puddle/item/forms.py
+++ puddle/item/forms.py
@@ -6,7 +6,7 @@ INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ['category','name','description','price','image',]
+        fields = ('category','name','description','price','image',)
         widgets = {
             'category': forms.Select(attrs={
                 'class': INPUT_CLASSES
@@ -21,6 +21,22 @@ class NewItemForm(forms.ModelForm):
                 'class': INPUT_CLASSES
             }),
             'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+        }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name','description','price', 'is_sold')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
             }),
         }
