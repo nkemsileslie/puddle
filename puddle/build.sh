cdf7fa249ef1d7162edb1ec3c857d428246187fe
nkemsileslie <nkemsileslie@gmail.com>
2024-02-24 15:41:04 -0500

    working updates

--- puddle/build.sh
+++ puddle/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
-
+# Exit on error
 set -o errexit
+
 
 pip install -r requirements.txt
 
-python manage.p collectstatic --no-input
+python manage.py collectstatic --no-input
+
 python manage.py migrate
